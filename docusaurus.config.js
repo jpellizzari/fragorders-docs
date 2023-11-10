1554dbe2e8f3cc1d88f930b728d6ab47aeda0798
Jordan Pellizzari <jordan@weave.works>
2023-11-10 19:46:46 +0000

    Add social jpg

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/fo-social.jpg',
       navbar: {
         title: 'Frag Orders Docs',
         logo: {
